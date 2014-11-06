a1ec28b38f5195aecb4ba8944c4371e7943aabc4
Jack Olney <jack.olney@me.com>
2014-11-06 17:39:58 +0000

    estimates and plot code

--- source/Run.R
+++ source/Run.R
@@ -29,13 +29,65 @@ plot(result$sPOP,type='l',lwd=2)
 plot(result$sHIV,type='l',lwd=2)
 plot(result$sART,type='l',lwd=2)
 
+
+# Figures
+graphics.off()
+quartz.options(h=10,w=10)
+library(RColorBrewer)
+p <- brewer.pal(9,"Set1")
+
 pop <- result$sPOP
 hiv <- result$sHIV
 art <- result$sART
 
+#Hiv Prevalence
 prev <- hiv / pop
-plot(prev,type='l',lwd=2)
-abline(v=33)
 
+Unaids_HivPrev <- read.csv("/Users/jack/git/CareCascade/estimates/UNAIDS_HivPrevalence_Kenya.csv",header=TRUE)
+Unaids_HivPrev$year
+
+plot(seq(0,59,1),prev,
+	type='l',
+	col=p[2],
+	lwd=2,
+	ylim=c(0,0.1),
+	main='HIV Prevalence',
+	xlab='Year',
+	ylab='Prevalence',
+	xaxt='n')
+lines(seq(20,42,1),Unaids_HivPrev$HIV_prev,
+	lwd=2,
+	lty=3,
+	col=p[1])
+axis(1,seq(0,60,5),seq(1970,2030,5))
+legend("topright",c("UNAIDS","CareCascade"),
+	fill=p[1:2],
+	box.lty=0,
+	border=NA,
+	cex=1.2)
+
+
+#PlwhivOnArt
 onart <- art / hiv
-plot(onart,type='l',lwd=2)
+
+Unaids_PlwhivOnArt <- read.csv("/Users/jack/git/CareCascade/estimates/UNAIDS_PlwhivOnArt_Kenya.csv",header=TRUE)
+
+plot(seq(0,59,1),onart,
+	type='l',
+	col=p[2],
+	lwd=2,
+	ylim=c(0,0.5),
+	main='Proportion of PLWHIV on ART'
+	xlab='Year',
+	ylab='Proportion',
+	xaxt='n')
+lines(seq(34,42,1),Unaids_PlwhivOnArt$prop_of_hiv_onart,
+	lwd=2,
+	lty=3,
+	col=p[1])
+axis(1,seq(0,60,5),seq(1970,2030,5))
+legend("topright",c("UNAIDS","CareCascade"),
+	fill=p[1:2],
+	box.lty=0,
+	border=NA,
+	cex=1.2)
