3dc4ba82bf300c80dd5bccf91ffd6303bc10b558
Jack Olney <jack.olney@me.com>
2014-12-03 12:07:24 +0000

    update

--- source/toolbox.cpp
+++ source/toolbox.cpp
@@ -18,13 +18,13 @@ extern Rng * theRng;
 
 /* Hiv testing times */
 double hctHivTestTime = 0.4329004 * 365.25;
-double vctHivTestTime = 10 * 365.25;
+double vctHivTestTime = 8 * 365.25;
 double pictHivTestTime_AsymptomaticNoCd4Result = 2 * 365.25;
-double pictHivTestTime_AsymptomaticCd4ResultNotEligible = 1 * 365.25;
-double pictHivTestTime_AsymptomaticCd4ResultEligible = 0.5 * 365.25;
-double pictHivTestTime_SymptomaticOblivious = 0.1 * 365.25;
-double pictHivTestTime_SymptomaticNoCd4Result = 0.5 * 365.25;
-double pictHivTestTime_SymptomaticCd4Result = 0.5 * 365.25;
+double pictHivTestTime_AsymptomaticCd4ResultNotEligible = 365.25;
+double pictHivTestTime_AsymptomaticCd4ResultEligible = 30;
+double pictHivTestTime_SymptomaticOblivious = 14;
+double pictHivTestTime_SymptomaticNoCd4Result = 14;
+double pictHivTestTime_SymptomaticCd4Result = 7;
 
 
 /* Linkage probabilities */
