bcd7f2b78a5bc6f4cb9f4230c7b4f3b99614b54d
Jack Olney <jack.olney@me.com>
2014-12-10 13:10:25 +0000

    update

--- source/toolbox.cpp
+++ source/toolbox.cpp
@@ -23,9 +23,9 @@ double pictHivTestTime_AsymptomaticOblivious = 15.8 * 365.25;
 double pictHivTestTime_AsymptomaticNoCd4Result = 13 * 365.25;
 double pictHivTestTime_AsymptomaticCd4ResultNotEligible = 6 * 365.25;
 double pictHivTestTime_AsymptomaticCd4ResultEligible = 6 * 365.25;
-double pictHivTestTime_SymptomaticOblivious = 2 * 365.25;
-double pictHivTestTime_SymptomaticNoCd4Result = 2 * 365.25;
-double pictHivTestTime_SymptomaticCd4Result = 2 * 365.25;
+double pictHivTestTime_SymptomaticOblivious = 2.5 * 365.25;
+double pictHivTestTime_SymptomaticNoCd4Result = 2.5 * 365.25;
+double pictHivTestTime_SymptomaticCd4Result = 2.5 * 365.25;
 
 
 /* Linkage probabilities */
