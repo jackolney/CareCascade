09ed4427e7ba068674ca48dc5b4b4db65dcc41ce
Jack Olney <jack.olney11@imperial.ac.uk>
2014-12-28 20:55:13 +0000

    calibration.cpp fix

--- source/calibration.h
+++ source/calibration.h
@@ -18,58 +18,10 @@ void SeedCalibration(person * const thePerson, const double theTimeZero, const d
 /////////////////////
 /////////////////////
 
-class TimeSplitZero : public event {
+class TimeSplit : public event {
 public:
-	TimeSplitZero(person * const thePerson, const double Time); //constructor
-	~TimeSplitZero(); //destructor
-	
-	/* Methods */
-	bool CheckValid();
-	void Execute();
-	
-private:
-	person * const pPerson;
-};
-
-/////////////////////
-/////////////////////
-
-class TimeSplitOne : public event {
-public:
-	TimeSplitOne(person * const thePerson, const double Time); //constructor
-	~TimeSplitOne(); //destructor
-	
-	/* Methods */
-	bool CheckValid();
-	void Execute();
-	
-private:
-	person * const pPerson;
-};
-
-/////////////////////
-/////////////////////
-
-class TimeSplitTwo : public event {
-public:
-	TimeSplitTwo(person * const thePerson, const double Time); //constructor
-	~TimeSplitTwo(); //destructor
-	
-	/* Methods */
-	bool CheckValid();
-	void Execute();
-	
-private:
-	person * const pPerson;
-};
-
-/////////////////////
-/////////////////////
-
-class TimeSplitThree : public event {
-public:
-	TimeSplitThree(person * const thePerson, const double Time); //constructor
-	~TimeSplitThree(); //destructor
+	TimeSplit(person * const thePerson, const double Time); //constructor
+	~TimeSplit(); //destructor
 	
 	/* Methods */
 	bool CheckValid();
@@ -84,10 +36,6 @@ private:
 
 void CreateCalibrationArray();
 
-void UpdateCalibrationArrayOne(person * const thePerson);
-
-void UpdateCalibrationArrayTwo(person * const thePerson);
-
-void UpdateCalibrationArrayThree(person * const thePerson);
+void UpdateCalibrationArray(person * const thePerson, const unsigned int theTimeIndex);
 
 #endif /* defined(__priorityQ__calibration__) */
