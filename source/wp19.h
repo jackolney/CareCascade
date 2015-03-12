e12a3254ac1e7df71abdc58208bd8a22e2245159
Jack Olney <jack.olney@me.com>
2015-03-12 14:41:09 +0000

    addition of wp19.h and wp19.cpp

---
+++ source/wp19.h
@@ -0,0 +1,44 @@
+//
+//  wp19.h
+//  priorityQ
+//
+//  Created by Jack Olney on 12/03/2015.
+//  Copyright (c) 2015 Jack Olney. All rights reserved.
+//
+
+#ifndef __priorityQ__wp19__
+#define __priorityQ__wp19__
+
+#include <stdio.h>
+#include "person.h"
+#include "event.h"
+
+void SeedGuidelinesOutput(person * const thePerson);
+
+/////////////////////
+/////////////////////
+
+class GuidelinesOutput : public event { //For reporting GuidelinesOutput from 2000 to 2035.
+public:
+	GuidelinesOutput(person * const thePerson, const double Time); //constructor
+	~GuidelinesOutput(); //destructor
+	
+	/* Methods */
+	bool CheckValid();
+	void Execute();
+	
+private:
+	person * const pPerson;
+};
+
+/////////////////////
+/////////////////////
+
+/* Population.cpp uses these functions. */
+
+void CreateGuidelinesArray();
+
+/////////////////////
+/////////////////////
+
+#endif /* defined(__priorityQ__wp19__) */
