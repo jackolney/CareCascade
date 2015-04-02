20c8eef38edfc71933e8b83e7ff7112e6a740faa
Jack Olney <jack.olney@me.com>
2015-04-02 16:40:19 +0100

    Edit to incorporate ever/never linked to care in WP19 output.

--- source/wp19Update.cpp
+++ source/wp19Update.cpp
@@ -17,78 +17,58 @@ extern eventQ * theQ;
 extern int * theGuidelines_PopDist_HivNegative;
 extern int * theGuidelines_PopDist_500_NeverDiag;
 extern int * theGuidelines_PopDist_500_DiagNotInCareNeverCare;
-extern int * theGuidelines_PopDist_500_DiagNotInCareNeverCareEligible;
 extern int * theGuidelines_PopDist_500_DiagNotInCareEverCare;
-extern int * theGuidelines_PopDist_500_DiagNotInCareEverCareEligible;
-extern int * theGuidelines_PopDist_500_InCareNeverArtNeverCd4Result;
-extern int * theGuidelines_PopDist_500_InCareNeverArtLessTwoCd4Test;
-extern int * theGuidelines_PopDist_500_InCareNeverArtMoreTwoCd4Test;
-extern int * theGuidelines_PopDist_500_ArtAtEnrollmentLessSixMonths;
-extern int * theGuidelines_PopDist_500_ArtAtEnrollmentMoreSixMonths;
+extern int * theGuidelines_PopDist_500_InCareNeverArt;
 extern int * theGuidelines_PopDist_500_ArtLessSixMonths;
 extern int * theGuidelines_PopDist_500_ArtMoreSixMonths;
 extern int * theGuidelines_PopDist_500_OffArt;
 extern int * theGuidelines_PopDist_350500_NeverDiag;
 extern int * theGuidelines_PopDist_350500_DiagNotInCareNeverCare;
-extern int * theGuidelines_PopDist_350500_DiagNotInCareNeverCareEligible;
 extern int * theGuidelines_PopDist_350500_DiagNotInCareEverCare;
-extern int * theGuidelines_PopDist_350500_DiagNotInCareEverCareEligible;
-extern int * theGuidelines_PopDist_350500_InCareNeverArtNeverCd4Result;
-extern int * theGuidelines_PopDist_350500_InCareNeverArtLessTwoCd4Test;
-extern int * theGuidelines_PopDist_350500_InCareNeverArtMoreTwoCd4Test;
-extern int * theGuidelines_PopDist_350500_ArtAtEnrollmentLessSixMonths;
-extern int * theGuidelines_PopDist_350500_ArtAtEnrollmentMoreSixMonths;
+extern int * theGuidelines_PopDist_350500_InCareNeverArt;
 extern int * theGuidelines_PopDist_350500_ArtLessSixMonths;
 extern int * theGuidelines_PopDist_350500_ArtMoreSixMonths;
 extern int * theGuidelines_PopDist_350500_OffArt;
 extern int * theGuidelines_PopDist_200350_NeverDiag;
 extern int * theGuidelines_PopDist_200350_DiagNotInCareNeverCare;
-extern int * theGuidelines_PopDist_200350_DiagNotInCareNeverCareEligible;
 extern int * theGuidelines_PopDist_200350_DiagNotInCareEverCare;
-extern int * theGuidelines_PopDist_200350_DiagNotInCareEverCareEligible;
-extern int * theGuidelines_PopDist_200350_InCareNeverArtNeverCd4Result;
-extern int * theGuidelines_PopDist_200350_InCareNeverArtLessTwoCd4Test;
-extern int * theGuidelines_PopDist_200350_InCareNeverArtMoreTwoCd4Test;
-extern int * theGuidelines_PopDist_200350_ArtAtEnrollmentLessSixMonths;
-extern int * theGuidelines_PopDist_200350_ArtAtEnrollmentMoreSixMonths;
+extern int * theGuidelines_PopDist_200350_InCareNeverArt;
 extern int * theGuidelines_PopDist_200350_ArtLessSixMonths;
 extern int * theGuidelines_PopDist_200350_ArtMoreSixMonths;
 extern int * theGuidelines_PopDist_200350_OffArt;
 extern int * theGuidelines_PopDist_200_NeverDiag;
 extern int * theGuidelines_PopDist_200_DiagNotInCareNeverCare;
-extern int * theGuidelines_PopDist_200_DiagNotInCareNeverCareEligible;
 extern int * theGuidelines_PopDist_200_DiagNotInCareEverCare;
-extern int * theGuidelines_PopDist_200_DiagNotInCareEverCareEligible;
-extern int * theGuidelines_PopDist_200_InCareNeverArtNeverCd4Result;
-extern int * theGuidelines_PopDist_200_InCareNeverArtLessTwoCd4Test;
-extern int * theGuidelines_PopDist_200_InCareNeverArtMoreTwoCd4Test;
-extern int * theGuidelines_PopDist_200_ArtAtEnrollmentLessSixMonths;
-extern int * theGuidelines_PopDist_200_ArtAtEnrollmentMoreSixMonths;
+extern int * theGuidelines_PopDist_200_InCareNeverArt;
 extern int * theGuidelines_PopDist_200_ArtLessSixMonths;
 extern int * theGuidelines_PopDist_200_ArtMoreSixMonths;
 extern int * theGuidelines_PopDist_200_OffArt;
 
 extern int * theGuidelines_Death_HivNegative;
 extern int * theGuidelines_Death_500_NeverDiag;
-extern int * theGuidelines_Death_500_DiagNotInCare;
+extern int * theGuidelines_Death_500_DiagNotInCareNeverCare;
+extern int * theGuidelines_Death_500_DiagNotInCareEverCare;
 extern int * theGuidelines_Death_500_InCareNeverArt;
 extern int * theGuidelines_Death_500_ArtLessSixMonths;
 extern int * theGuidelines_Death_500_ArtMoreSixMonths;
 extern int * theGuidelines_Death_500_OffArt;
 extern int * theGuidelines_Death_350500_NeverDiag;
-extern int * theGuidelines_Death_350500_DiagNotInCare;
+extern int * theGuidelines_Death_350500_DiagNotInCareNeverCare;
+extern int * theGuidelines_Death_350500_DiagNotInCareEverCare;
 extern int * theGuidelines_Death_350500_InCareNeverArt;
 extern int * theGuidelines_Death_350500_ArtLessSixMonths;
 extern int * theGuidelines_Death_350500_ArtMoreSixMonths;
 extern int * theGuidelines_Death_350500_OffArt;
 extern int * theGuidelines_Death_200350_NeverDiag;
-extern int * theGuidelines_Death_200350_DiagNotInCare;
+extern int * theGuidelines_Death_200350_DiagNotInCareNeverCare;
+extern int * theGuidelines_Death_200350_DiagNotInCareEverCare;
 extern int * theGuidelines_Death_200350_InCareNeverArt;
 extern int * theGuidelines_Death_200350_ArtLessSixMonths;
 extern int * theGuidelines_Death_200350_ArtMoreSixMonths;
 extern int * theGuidelines_Death_200350_OffArt;
 extern int * theGuidelines_Death_200_NeverDiag;
-extern int * theGuidelines_Death_200_DiagNotInCare;
+extern int * theGuidelines_Death_200_DiagNotInCareNeverCare;
+extern int * theGuidelines_Death_200_DiagNotInCareEverCare;
 extern int * theGuidelines_Death_200_InCareNeverArt;
 extern int * theGuidelines_Death_200_ArtLessSixMonths;
 extern int * theGuidelines_Death_200_ArtMoreSixMonths;
@@ -114,24 +94,12 @@ void WriteGuidelinesPopDist(person * const thePerson, const size_t theIndex)
 	} else if(thePerson->GetCurrentCd4() == 4) {
 		if(!thePerson->GetDiagnosedState())
 			theGuidelines_PopDist_500_NeverDiag[theIndex]++;
-		else if(!thePerson->GetInCareState() && !thePerson->GetEverCareState() && !thePerson->GetEligible())
+		else if(!thePerson->GetInCareState() && !thePerson->GetEverCareState())
 			theGuidelines_PopDist_500_DiagNotInCareNeverCare[theIndex]++;
-		else if(!thePerson->GetInCareState() && !thePerson->GetEverCareState() && thePerson->GetEligible())
-			theGuidelines_PopDist_500_DiagNotInCareNeverCareEligible[theIndex]++;
-		else if(!thePerson->GetInCareState() && thePerson->GetEverCareState() && !thePerson->GetEligible())
+		else if(!thePerson->GetInCareState() && thePerson->GetEverCareState())
 			theGuidelines_PopDist_500_DiagNotInCareEverCare[theIndex]++;
-		else if(!thePerson->GetInCareState() && thePerson->GetEverCareState() && thePerson->GetEligible())
-			theGuidelines_PopDist_500_DiagNotInCareEverCareEligible[theIndex]++;
-		else if(!thePerson->GetEverArt() && !thePerson->GetEverCd4TestResultState())
-			theGuidelines_PopDist_500_InCareNeverArtNeverCd4Result[theIndex]++;
-		else if(!thePerson->GetEverArt() && thePerson->GetCd4TestCount() <= 2)
-			theGuidelines_PopDist_500_InCareNeverArtLessTwoCd4Test[theIndex]++;
-		else if(!thePerson->GetEverArt() && thePerson->GetCd4TestCount() > 2)	
-			theGuidelines_PopDist_500_InCareNeverArtMoreTwoCd4Test[theIndex]++;
-		else if(thePerson->GetArtInitiationState() && thePerson->GetArtAtEnrollment() && thePerson->GetArtDay() >= (theQ->GetTime() - 182.625))		
-			theGuidelines_PopDist_500_ArtAtEnrollmentLessSixMonths[theIndex]++;
-		else if(thePerson->GetArtInitiationState() && thePerson->GetArtAtEnrollment() && thePerson->GetArtDay() < (theQ->GetTime() - 182.625))
-			theGuidelines_PopDist_500_ArtAtEnrollmentMoreSixMonths[theIndex]++;
+		else if(!thePerson->GetEverArt())
+			theGuidelines_PopDist_500_InCareNeverArt[theIndex]++;
 		else if(thePerson->GetArtInitiationState() && thePerson->GetArtDay() >= (theQ->GetTime() - 182.625))
 			theGuidelines_PopDist_500_ArtLessSixMonths[theIndex]++;
 		else if(thePerson->GetArtInitiationState() && thePerson->GetArtDay() < (theQ->GetTime() - 182.625))
@@ -141,51 +109,27 @@ void WriteGuidelinesPopDist(person * const thePerson, const size_t theIndex)
 	} else if(thePerson->GetCurrentCd4() == 3) {
 		if(!thePerson->GetDiagnosedState())
 			theGuidelines_PopDist_350500_NeverDiag[theIndex]++;
-		else if(!thePerson->GetInCareState() && !thePerson->GetEverCareState() && !thePerson->GetEligible())
+		else if(!thePerson->GetInCareState() && !thePerson->GetEverCareState())
 			theGuidelines_PopDist_350500_DiagNotInCareNeverCare[theIndex]++;
-		else if(!thePerson->GetInCareState() && !thePerson->GetEverCareState() && thePerson->GetEligible())
-			theGuidelines_PopDist_350500_DiagNotInCareNeverCareEligible[theIndex]++;
-		else if(!thePerson->GetInCareState() && thePerson->GetEverCareState() && !thePerson->GetEligible())
+		else if(!thePerson->GetInCareState() && thePerson->GetEverCareState())
 			theGuidelines_PopDist_350500_DiagNotInCareEverCare[theIndex]++;
-		else if(!thePerson->GetInCareState() && thePerson->GetEverCareState() && thePerson->GetEligible())
-			theGuidelines_PopDist_350500_DiagNotInCareEverCareEligible[theIndex]++;
-		else if(!thePerson->GetEverArt() && !thePerson->GetEverCd4TestResultState())
-			theGuidelines_PopDist_350500_InCareNeverArtNeverCd4Result[theIndex]++;
-		else if(!thePerson->GetEverArt() && thePerson->GetCd4TestCount() <= 2)
-			theGuidelines_PopDist_350500_InCareNeverArtLessTwoCd4Test[theIndex]++;
-		else if(!thePerson->GetEverArt() && thePerson->GetCd4TestCount() > 2)	
-			theGuidelines_PopDist_350500_InCareNeverArtMoreTwoCd4Test[theIndex]++;
-		else if(thePerson->GetArtInitiationState() && thePerson->GetArtAtEnrollment() && thePerson->GetArtDay() >= (theQ->GetTime() - 182.625))		
-			theGuidelines_PopDist_350500_ArtAtEnrollmentLessSixMonths[theIndex]++;
-		else if(thePerson->GetArtInitiationState() && thePerson->GetArtAtEnrollment() && thePerson->GetArtDay() < (theQ->GetTime() - 182.625))
-			theGuidelines_PopDist_350500_ArtAtEnrollmentMoreSixMonths[theIndex]++;
+		else if(!thePerson->GetEverArt())
+			theGuidelines_PopDist_350500_InCareNeverArt[theIndex]++;
 		else if(thePerson->GetArtInitiationState() && thePerson->GetArtDay() >= (theQ->GetTime() - 182.625))
 			theGuidelines_PopDist_350500_ArtLessSixMonths[theIndex]++;
 		else if(thePerson->GetArtInitiationState() && thePerson->GetArtDay() < (theQ->GetTime() - 182.625))
 			theGuidelines_PopDist_350500_ArtMoreSixMonths[theIndex]++;
 		else if(!thePerson->GetArtInitiationState())
-			theGuidelines_PopDist_350500_OffArt[theIndex]++;	
+			theGuidelines_PopDist_350500_OffArt[theIndex]++;
 	} else if(thePerson->GetCurrentCd4() == 2) {
 		if(!thePerson->GetDiagnosedState())
 			theGuidelines_PopDist_200350_NeverDiag[theIndex]++;
-		else if(!thePerson->GetInCareState() && !thePerson->GetEverCareState() && !thePerson->GetEligible())
+		else if(!thePerson->GetInCareState() && !thePerson->GetEverCareState())
 			theGuidelines_PopDist_200350_DiagNotInCareNeverCare[theIndex]++;
-		else if(!thePerson->GetInCareState() && !thePerson->GetEverCareState() && thePerson->GetEligible())
-			theGuidelines_PopDist_200350_DiagNotInCareNeverCareEligible[theIndex]++;
-		else if(!thePerson->GetInCareState() && thePerson->GetEverCareState() && !thePerson->GetEligible())
+		else if(!thePerson->GetInCareState() && thePerson->GetEverCareState())
 			theGuidelines_PopDist_200350_DiagNotInCareEverCare[theIndex]++;
-		else if(!thePerson->GetInCareState() && thePerson->GetEverCareState() && thePerson->GetEligible())
-			theGuidelines_PopDist_200350_DiagNotInCareEverCareEligible[theIndex]++;
-		else if(!thePerson->GetEverArt() && !thePerson->GetEverCd4TestResultState())
-			theGuidelines_PopDist_200350_InCareNeverArtNeverCd4Result[theIndex]++;
-		else if(!thePerson->GetEverArt() && thePerson->GetCd4TestCount() <= 2)
-			theGuidelines_PopDist_200350_InCareNeverArtLessTwoCd4Test[theIndex]++;
-		else if(!thePerson->GetEverArt() && thePerson->GetCd4TestCount() > 2)	
-			theGuidelines_PopDist_200350_InCareNeverArtMoreTwoCd4Test[theIndex]++;
-		else if(thePerson->GetArtInitiationState() && thePerson->GetArtAtEnrollment() && thePerson->GetArtDay() >= (theQ->GetTime() - 182.625))		
-			theGuidelines_PopDist_200350_ArtAtEnrollmentLessSixMonths[theIndex]++;
-		else if(thePerson->GetArtInitiationState() && thePerson->GetArtAtEnrollment() && thePerson->GetArtDay() < (theQ->GetTime() - 182.625))
-			theGuidelines_PopDist_200350_ArtAtEnrollmentMoreSixMonths[theIndex]++;
+		else if(!thePerson->GetEverArt())
+			theGuidelines_PopDist_200350_InCareNeverArt[theIndex]++;
 		else if(thePerson->GetArtInitiationState() && thePerson->GetArtDay() >= (theQ->GetTime() - 182.625))
 			theGuidelines_PopDist_200350_ArtLessSixMonths[theIndex]++;
 		else if(thePerson->GetArtInitiationState() && thePerson->GetArtDay() < (theQ->GetTime() - 182.625))
@@ -195,24 +139,12 @@ void WriteGuidelinesPopDist(person * const thePerson, const size_t theIndex)
 	} else if(thePerson->GetCurrentCd4() == 1) {
 		if(!thePerson->GetDiagnosedState())
 			theGuidelines_PopDist_200_NeverDiag[theIndex]++;
-		else if(!thePerson->GetInCareState() && !thePerson->GetEverCareState() && !thePerson->GetEligible())
+		else if(!thePerson->GetInCareState() && !thePerson->GetEverCareState())
 			theGuidelines_PopDist_200_DiagNotInCareNeverCare[theIndex]++;
-		else if(!thePerson->GetInCareState() && !thePerson->GetEverCareState() && thePerson->GetEligible())
-			theGuidelines_PopDist_200_DiagNotInCareNeverCareEligible[theIndex]++;
-		else if(!thePerson->GetInCareState() && thePerson->GetEverCareState() && !thePerson->GetEligible())
+		else if(!thePerson->GetInCareState() && thePerson->GetEverCareState())
 			theGuidelines_PopDist_200_DiagNotInCareEverCare[theIndex]++;
-		else if(!thePerson->GetInCareState() && thePerson->GetEverCareState() && thePerson->GetEligible())
-			theGuidelines_PopDist_200_DiagNotInCareEverCareEligible[theIndex]++;
-		else if(!thePerson->GetEverArt() && !thePerson->GetEverCd4TestResultState())
-			theGuidelines_PopDist_200_InCareNeverArtNeverCd4Result[theIndex]++;
-		else if(!thePerson->GetEverArt() && thePerson->GetCd4TestCount() <= 2)
-			theGuidelines_PopDist_200_InCareNeverArtLessTwoCd4Test[theIndex]++;
-		else if(!thePerson->GetEverArt() && thePerson->GetCd4TestCount() > 2)	
-			theGuidelines_PopDist_200_InCareNeverArtMoreTwoCd4Test[theIndex]++;
-		else if(thePerson->GetArtInitiationState() && thePerson->GetArtAtEnrollment() && thePerson->GetArtDay() >= (theQ->GetTime() - 182.625))		
-			theGuidelines_PopDist_200_ArtAtEnrollmentLessSixMonths[theIndex]++;
-		else if(thePerson->GetArtInitiationState() && thePerson->GetArtAtEnrollment() && thePerson->GetArtDay() < (theQ->GetTime() - 182.625))
-			theGuidelines_PopDist_200_ArtAtEnrollmentMoreSixMonths[theIndex]++;
+		else if(!thePerson->GetEverArt())
+			theGuidelines_PopDist_200_InCareNeverArt[theIndex]++;
 		else if(thePerson->GetArtInitiationState() && thePerson->GetArtDay() >= (theQ->GetTime() - 182.625))
 			theGuidelines_PopDist_200_ArtLessSixMonths[theIndex]++;
 		else if(thePerson->GetArtInitiationState() && thePerson->GetArtDay() < (theQ->GetTime() - 182.625))
@@ -241,8 +173,10 @@ void WriteGuidelinesDeath(person * const thePerson)
 		} else if(thePerson->GetCurrentCd4() == 4) {
 			if(!thePerson->GetDiagnosedState())
 				theGuidelines_Death_500_NeverDiag[i]++;
-			else if(!thePerson->GetInCareState())
-				theGuidelines_Death_500_DiagNotInCare[i]++;
+			else if(!thePerson->GetInCareState() && !thePerson->GetEverCareState())
+				theGuidelines_Death_500_DiagNotInCareNeverCare[i]++;
+			else if(!thePerson->GetInCareState() && thePerson->GetEverCareState())
+				theGuidelines_Death_500_DiagNotInCareEverCare[i]++;
 			else if(!thePerson->GetEverArt())
 				theGuidelines_Death_500_InCareNeverArt[i]++;
 			else if(thePerson->GetArtInitiationState() && thePerson->GetArtDay() >= (theQ->GetTime() - 182.625))
@@ -250,12 +184,14 @@ void WriteGuidelinesDeath(person * const thePerson)
 			else if(thePerson->GetArtInitiationState() && thePerson->GetArtDay() < (theQ->GetTime() - 182.625))
 				theGuidelines_Death_500_ArtMoreSixMonths[i]++;
 			else if(!thePerson->GetArtInitiationState())
-				theGuidelines_Death_500_OffArt[i]++;		
+				theGuidelines_Death_500_OffArt[i]++;
 		} else if(thePerson->GetCurrentCd4() == 3) {
 			if(!thePerson->GetDiagnosedState())
 				theGuidelines_Death_350500_NeverDiag[i]++;
-			else if(!thePerson->GetInCareState())
-				theGuidelines_Death_350500_DiagNotInCare[i]++;
+			else if(!thePerson->GetInCareState() && !thePerson->GetEverCareState())
+				theGuidelines_Death_350500_DiagNotInCareNeverCare[i]++;
+			else if(!thePerson->GetInCareState() && thePerson->GetEverCareState())
+				theGuidelines_Death_350500_DiagNotInCareEverCare[i]++;
 			else if(!thePerson->GetEverArt())
 				theGuidelines_Death_350500_InCareNeverArt[i]++;
 			else if(thePerson->GetArtInitiationState() && thePerson->GetArtDay() >= (theQ->GetTime() - 182.625))
@@ -267,8 +203,10 @@ void WriteGuidelinesDeath(person * const thePerson)
 		} else if(thePerson->GetCurrentCd4() == 2) {
 			if(!thePerson->GetDiagnosedState())
 				theGuidelines_Death_200350_NeverDiag[i]++;
-			else if(!thePerson->GetInCareState())
-				theGuidelines_Death_200350_DiagNotInCare[i]++;
+			else if(!thePerson->GetInCareState() && !thePerson->GetEverCareState())
+				theGuidelines_Death_200350_DiagNotInCareNeverCare[i]++;
+			else if(!thePerson->GetInCareState() && thePerson->GetEverCareState())
+				theGuidelines_Death_200350_DiagNotInCareEverCare[i]++;
 			else if(!thePerson->GetEverArt())
 				theGuidelines_Death_200350_InCareNeverArt[i]++;
 			else if(thePerson->GetArtInitiationState() && thePerson->GetArtDay() >= (theQ->GetTime() - 182.625))
@@ -280,8 +218,10 @@ void WriteGuidelinesDeath(person * const thePerson)
 		} else if(thePerson->GetCurrentCd4() == 1) {
 			if(!thePerson->GetDiagnosedState())
 				theGuidelines_Death_200_NeverDiag[i]++;
-			else if(!thePerson->GetInCareState())
-				theGuidelines_Death_200_DiagNotInCare[i]++;
+			else if(!thePerson->GetInCareState() && !thePerson->GetEverCareState())
+				theGuidelines_Death_200_DiagNotInCareNeverCare[i]++;
+			else if(!thePerson->GetInCareState() && thePerson->GetEverCareState())
+				theGuidelines_Death_200_DiagNotInCareEverCare[i]++;
 			else if(!thePerson->GetEverArt())
 				theGuidelines_Death_200_InCareNeverArt[i]++;
 			else if(thePerson->GetArtInitiationState() && thePerson->GetArtDay() >= (theQ->GetTime() - 182.625))
